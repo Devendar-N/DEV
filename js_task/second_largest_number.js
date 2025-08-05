934b0dc0b789474303565faa5cd33835b596b776
Devendar-N <devendar.n@numentica-ui.com>
2025-08-05 22:51:51 +0530

    modified:   js_task/second_largest_number.js

--- js_task/second_largest_number.js
+++ js_task/second_largest_number.js
@@ -2,20 +2,18 @@
 Input: [1, 45, 32, 76, 23]
 Output: 45*/
 const input = [1, 45, 32, 76, 23]
-function finding_second_largest_number(number){
+function finding_second_largest_number(number){  //Function to find second largest largest number
     const length_of_number = number.length
-    let swapped ;
-    do{
-        swapped = false;
-        for(let i = 0 ; i < length_of_number-1 ; i++){
-            if(number [i]< input[i+1]){
-                let temporary = number[i] ;
-                number[i] = number[i + 1] ;
-                number[ i + 1] = temporary ;
-                swapped = true ;
+    for(let i = 0; i < length_of_number ; i++) {  // Outer loop
+        for(let j = 0 ; j < length_of_number ; j++){   // Inner loop
+            if(number [j] < number [j+1]){
+                // If current element is smaller then swap next element
+                let temporary = number[j] ;
+                number[j] = number[j + 1] ;
+                number[ j + 1] = temporary ;
             }
         }
-    }while(swapped) ;
-    console.log("The second largest number is = ", number[1] ) ;
+    }
+    console.log("The second largest number is = ", number[1] ) ;  // Display the output
 }
 finding_second_largest_number(input)
