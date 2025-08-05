a2cf5c8cc17a57291ab78b2107524c9234eb42c4
Devendar-N <devendar.n@numentica-ui.com>
2025-08-05 21:18:43 +0530

    modified:   js_task/1double.js

--- js_task/1double.js
+++ js_task/1double.js
@@ -1,18 +1,18 @@
 /*Create a function that doubles the even numbers in an array.
 Input: [2, 5, 8, 9, 4]
 Output: [4, 5, 16, 9, 8]*/
-const output=[];
-const input_array=[2, 5, 8, 9, 4];
-function multiple_even_numbers_in_array(){
-    for(let i=0;i<input_array.length;i++){
-    if(input_array[i]%2==0){
-        output[i]=input_array[i]*2;
+
+const output = []; // Variable to store the output value
+const input_array = [2, 5, 8, 9, 4];
+function multiple_even_numbers_in_array(array){    // Function to multiple the even numbers in a array
+    for(let i = 0 ; i < array.length ; i++){
+    if( array[i] % 2 == 0 ){
+        output[i] = array[i] * 2 ;        // Multiplying the even elements
     }
     else{
-        output[i]=input_array[i];
+        output[i] = array[i] ;
     }
-    manual_index++;
 }
-console.log("Output after multiplying even Numbers in a Array :",output);
+console.log("Output after multiplying even Numbers in a Array :" , output);      // Displaying the final output
 }
-multiple_even_numbers_in_array()
+multiple_even_numbers_in_array(input_array)
--- js_task/2count.js
+++ js_task/2count.js
@@ -3,16 +3,23 @@ Input: "numentica ui"
 Output: 6*/
 
 const input = "numentica ui"
-const array = input.split("")
-function convert(){
-    const vowels = ['a','e','i','o','u']
-    let count = 0
-    const length=array.length
-    for(let i=0; i < length;i++){
-        if(vowels.includes(array[i])){
-             count++
+let output_array=[]
+function convert_to_array(array){   //Function that convert a string to array
+    for(let i=0;i<array.length;i++){
+        output_array[i]=array[i]
     }
 }
-    console.log("The number of vowels in a string :",count)
+function count_vowels(value){
+    let vowels_count=0;
+    for(let j=0;j<value.length;j++){
+        if(value[j] == 'a' || value[j] == 'e' || value[j] == 'i' || value[j] == 'o' || value[j] == 'u' ){
+            vowels_count += 1;
+        }
+        else{
+            continue;
+        }
+    }
+    console.log('Number of vowels in ',input , '=', vowels_count)
 }
-convert()
+convert_to_array(input)// calling the function
+count_vowels(output_array)
