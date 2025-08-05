bd679f58c4cd236cd0a80137f2a640dd112f7f8b
Devendar-N <devendar.n@numentica-ui.com>
2025-08-05 21:30:57 +0530

    modified:   js_task/2count.js

--- js_task/2count.js
+++ js_task/2count.js
@@ -3,23 +3,20 @@ Input: "numentica ui"
 Output: 6*/
 
 const input = "numentica ui"
-let output_array=[]
+let converted_array = []
 function convert_to_array(array){   //Function that convert a string to array
-    for(let i=0;i<array.length;i++){
-        output_array[i]=array[i]
+    for(let i = 0 ; i < array.length ; i++){
+        converted_array[i] = array[i]  // Storing the converted array in output_array variable
     }
 }
-function count_vowels(value){
-    let vowels_count=0;
-    for(let j=0;j<value.length;j++){
+function count_vowels(value){   // Function that counts number of vowels in given input
+    let vowels_count = 0;       
+    for(let j = 0 ; j < value.length ; j++){
         if(value[j] == 'a' || value[j] == 'e' || value[j] == 'i' || value[j] == 'o' || value[j] == 'u' ){
-            vowels_count += 1;
-        }
-        else{
-            continue;
+            vowels_count += 1 ;    
         }
     }
-    console.log('Number of vowels in ',input , '=', vowels_count)
+    console.log('Number of vowels in ', input , ' = ', vowels_count) // Display the output
 }
-convert_to_array(input)// calling the function
-count_vowels(output_array)
+convert_to_array(input)
+count_vowels(converted_array)
