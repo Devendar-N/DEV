8826593fc53f7bfe1fa4c2a930bf721e0b04ec11
Devendar-N <devendar.n@numentica-ui.com>
2025-08-05 21:58:10 +0530

    new file:   js_task/second_largest_number.js

---
+++ js_task/second_largest_number.js
@@ -0,0 +1,21 @@
+/*3. Problem: Create a function that returns the second largest number in an array.
+Input: [1, 45, 32, 76, 23]
+Output: 45*/
+const input = [1, 45, 32, 76, 23]
+function finding_second_largest_number(number){
+    const length_of_number = number.length
+    let swapped ;
+    do{
+        swapped = false;
+        for(let i = 0 ; i < length_of_number-1 ; i++){
+            if(number [i]< input[i+1]){
+                let temporary = number[i] ;
+                number[i] = number[i + 1] ;
+                number[ i + 1] = temporary ;
+                swapped = true ;
+            }
+        }
+    }while(swapped);
+    return number ;
+}
+finding_second_largest_number(input)
